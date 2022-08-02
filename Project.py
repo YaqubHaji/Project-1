83be065936082db3ea79e009b224ec49e783ea3d
Yaqub <yaqubhaji10@gmail.com>
2022-08-02 21:42:35 +0100

    Bookmark commit Project-1

--- Project.py
+++ Project.py
@@ -1,3 +1,6 @@
-def project:
-    print(pp)
-    print("Yaqub")
+import webbrowser
+
+with open('websites.txt') as file:
+    links = file.readlines()
+    for link in links:
+        webbrowser.open(link)
